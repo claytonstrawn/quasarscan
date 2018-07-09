d1e9299735b63e780d1a26d82c6e2c0023a1622e
Clayton Strawn <cstrawn@cori12.nersc.gov>
2018-07-09 10:41:02 -0700

    Adding the bash script to run files stored in Daniel Ceverino's files on HSI

---
+++ run_one_snapshot_nersc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#from VELA01 to VELA07, then VELA16 to VELA35
+#10MpcBox_csf512_a0.020.d 
+#from a0.330 to a0.500
+
+SOURCEDIR="/home/c/ceverino/VELA"
+
+if [[ $1 = "1" ]]
+then
+    v=""
+    DIRNAME=$SOURCEDIR
+else
+    v="_v"$1"_"
+    DIRNAME=$SOURCEDIR"_v"$1
+fi
+i=$2
+a=$3
+
+mkdir "VELA"$v$i
+cd "VELA"$v$i
+hsi "cd "$DIRNAME"/VELA"$i"; get *a0.$a*"
+cd ..
+
+module load python
+source activate myenv
+
+python quasarscan/quasar_scan.py 'n' 'VELA'$v$i'/10MpcBox_csf512_a0.'$a'.d' 'VELA'$v$i '-p'
+
+rm -rf 'VELA'$v$i
+source deactivate
