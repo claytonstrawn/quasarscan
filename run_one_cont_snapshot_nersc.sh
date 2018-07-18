b901cff169ada1ab2de7e1ab2f87dd63c9259198
Clayton Strawn <cstrawn@cori04.nersc.gov>
2018-07-18 14:03:44 -0700

    added functionality for new/continuation scripts

---
+++ run_one_cont_snapshot_nersc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+i=$2
+z=$3
+
+v="_v"$1"_"
+cd "VELA"$v$i
+ls
+cd ..
+
+module load python
+source activate myenv
+
+python quasarscan/quasar_scan.py 'c' '-sz' 'VELA$v$i' '$z' '-p' '-s' 12
+
+rm -rf 'VELA'$v$i
+source deactivate
