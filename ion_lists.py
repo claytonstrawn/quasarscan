1334559449ed1694d828c2680f7712be0f380469
Clayton Strawn <cjstrawn@ucsc.edu>
2018-08-29 15:41:30 -0700

    track version number on non-binning branch

--- ion_lists.py
+++ ion_lists.py
@@ -1,3 +1,60 @@
-truly_allions = ['Al I', 'Al XI', 'Al XII', 'Al XIII', 'Al XIV', 'Al II', 'Al III', 'Al IV', 'Al V', 'Al VI', 'Al VII', 'Al VIII', 'Al IX', 'Al X', 'Ar I', 'Ar XI', 'Ar XII', 'Ar XIII', 'Ar XIV', 'Ar XV', 'Ar XVI', 'Ar XVII', 'Ar XVIII', 'Ar XIX', 'Ar II', 'Ar III', 'Ar IV', 'Ar V', 'Ar VI', 'Ar VII', 'Ar VIII', 'Ar IX', 'Ar X', 'B I', 'B II', 'B III', 'B IV', 'B V', 'B VI', 'Be I', 'Be II', 'Be III', 'Be IV', 'Be V', 'C I', 'C II', 'C III', 'C IV', 'C V', 'C VI', 'C VII', 'Ca I', 'Ca XI', 'Ca XII', 'Ca XIII', 'Ca XIV', 'Ca XV', 'Ca XVI', 'Ca XVII', 'Ca XVIII', 'Ca XIX', 'Ca XX', 'Ca II', 'Ca XXI', 'Ca III', 'Ca IV', 'Ca V', 'Ca VI', 'Ca VII', 'Ca VIII', 'Ca IX', 'Ca X', 'Cl I', 'Cl XI', 'Cl XII', 'Cl XIII', 'Cl XIV', 'Cl XV', 'Cl XVI', 'Cl XVII', 'Cl XVIII', 'Cl II', 'Cl III', 'Cl IV', 'Cl V', 'Cl VI', 'Cl VII', 'Cl VIII', 'Cl IX', 'Cl X', 'Co I', 'Co XI', 'Co XII', 'Co XIII', 'Co XIV', 'Co XV', 'Co XVI', 'Co XVII', 'Co XVIII', 'Co XIX', 'Co XX', 'Co II', 'Co XXI', 'Co XXII', 'Co XXIII', 'Co XXIV', 'Co XXV', 'Co XXVI', 'Co XXVII', 'Co XXVIII', 'Co III', 'Co IV', 'Co V', 'Co VI', 'Co VII', 'Co VIII', 'Co IX', 'Co X', 'Cr I', 'Cr XI', 'Cr XII', 'Cr XIII', 'Cr XIV', 'Cr XV', 'Cr XVI', 'Cr XVII', 'Cr XVIII', 'Cr XIX', 'Cr XX', 'Cr II', 'Cr XXI', 'Cr XXII', 'Cr XXIII', 'Cr XXIV', 'Cr XXV', 'Cr III', 'Cr IV', 'Cr V', 'Cr VI', 'Cr VII', 'Cr VIII', 'Cr IX', 'Cr X', 'Cu I', 'Cu XI', 'Cu XII', 'Cu XIII', 'Cu XIV', 'Cu XV', 'Cu XVI', 'Cu XVII', 'Cu XVIII', 'Cu XIX', 'Cu XX', 'Cu II', 'Cu XXI', 'Cu XXII', 'Cu XXIII', 'Cu XXIV', 'Cu XXV', 'Cu XXVI', 'Cu XXVII', 'Cu XXVIII', 'Cu XXIX', 'Cu XXX', 'Cu III', 'Cu IV', 'Cu V', 'Cu VI', 'Cu VII', 'Cu VIII', 'Cu IX', 'Cu X', 'F I', 'F II', 'F III', 'F IV', 'F V', 'F VI', 'F VII', 'F VIII', 'F IX', 'F X', 'Fe I', 'Fe XI', 'Fe XII', 'Fe XIII', 'Fe XIV', 'Fe XV', 'Fe XVI', 'Fe XVII', 'Fe XVIII', 'Fe XIX', 'Fe XX', 'Fe II', 'Fe XXI', 'Fe XXII', 'Fe XXIII', 'Fe XXIV', 'Fe XXV', 'Fe XXVI', 'Fe XXVII', 'Fe III', 'Fe IV', 'Fe V', 'Fe VI', 'Fe VII', 'Fe VIII', 'Fe IX', 'Fe X', 'H I', 'H II', 'He I', 'He II', 'He III', 'K I', 'K XI', 'K XII', 'K XIII', 'K XIV', 'K XV', 'K XVI', 'K XVII', 'K XVIII', 'K XIX', 'K XX', 'K II', 'K III', 'K IV', 'K V', 'K VI', 'K VII', 'K VIII', 'K IX', 'K X', 'Li I', 'Li II', 'Li III', 'Li IV', 'Mg I', 'Mg XI', 'Mg XII', 'Mg XIII', 'Mg II', 'Mg III', 'Mg IV', 'Mg V', 'Mg VI', 'Mg VII', 'Mg VIII', 'Mg IX', 'Mg X', 'Mn I', 'Mn XI', 'Mn XII', 'Mn XIII', 'Mn XIV', 'Mn XV', 'Mn XVI', 'Mn XVII', 'Mn XVIII', 'Mn XIX', 'Mn XX', 'Mn II', 'Mn XXI', 'Mn XXII', 'Mn XXIII', 'Mn XXIV', 'Mn XXV', 'Mn XXVI', 'Mn III', 'Mn IV', 'Mn V', 'Mn VI', 'Mn VII', 'Mn VIII', 'Mn IX', 'Mn X', 'N I', 'N II', 'N III', 'N IV', 'N V', 'N VI', 'N VII', 'N VIII', 'Na I', 'Na XI', 'Na XII', 'Na II', 'Na III', 'Na IV', 'Na V', 'Na VI', 'Na VII', 'Na VIII', 'Na IX', 'Na X', 'Ne I', 'Ne XI', 'Ne II', 'Ne III', 'Ne IV', 'Ne V', 'Ne VI', 'Ne VII', 'Ne VIII', 'Ne IX', 'Ne X', 'Ni I', 'Ni XI', 'Ni XII', 'Ni XIII', 'Ni XIV', 'Ni XV', 'Ni XVI', 'Ni XVII', 'Ni XVIII', 'Ni XIX', 'Ni XX', 'Ni II', 'Ni XXI', 'Ni XXII', 'Ni XXIII', 'Ni XXIV', 'Ni XXV', 'Ni XXVI', 'Ni XXVII', 'Ni XXVIII', 'Ni XXIX', 'Ni III', 'Ni IV', 'Ni V', 'Ni VI', 'Ni VII', 'Ni VIII', 'Ni IX', 'Ni X', 'O I', 'O II', 'O III', 'O IV', 'O V', 'O VI', 'O VII', 'O VIII', 'O IX', 'P I', 'P XI', 'P XII', 'P XIII', 'P XIV', 'P XV', 'P XVI', 'P II', 'P III', 'P IV', 'P V', 'P VI', 'P VII', 'P VIII', 'P IX', 'P X', 'S I', 'S XI', 'S XII', 'S XIII', 'S XIV', 'S XV', 'S XVI', 'S XVII', 'S II', 'S III', 'S IV', 'S V', 'S VI', 'S VII', 'S VIII', 'S IX', 'S X', 'Sc I', 'Sc XI', 'Sc XII', 'Sc XIII', 'Sc XIV', 'Sc XV', 'Sc XVI', 'Sc XVII', 'Sc XVIII', 'Sc XIX', 'Sc XX', 'Sc II', 'Sc XXI', 'Sc XXII', 'Sc III', 'Sc IV', 'Sc V', 'Sc VI', 'Sc VII', 'Sc VIII', 'Sc IX', 'Sc X', 'Si I', 'Si XI', 'Si XII', 'Si XIII', 'Si XIV', 'Si XV', 'Si II', 'Si III', 'Si IV', 'Si V', 'Si VI', 'Si VII', 'Si VIII', 'Si IX', 'Si X', 'Ti I', 'Ti XI', 'Ti XII', 'Ti XIII', 'Ti XIV', 'Ti XV', 'Ti XVI', 'Ti XVII', 'Ti XVIII', 'Ti XIX', 'Ti XX', 'Ti II', 'Ti XXI', 'Ti XXII', 'Ti XXIII', 'Ti III', 'Ti IV', 'Ti V', 'Ti VI', 'Ti VII', 'Ti VIII', 'Ti IX', 'Ti X', 'V I', 'V XI', 'V XII', 'V XIII', 'V XIV', 'V XV', 'V XVI', 'V XVII', 'V XVIII', 'V XIX', 'V XX', 'V II', 'V XXI', 'V XXII', 'V XXIII', 'V XXIV', 'V III', 'V IV', 'V V', 'V VI', 'V VII', 'V VIII', 'V IX', 'V X', 'Zn I', 'Zn XI', 'Zn XII', 'Zn XIII', 'Zn XIV', 'Zn XV', 'Zn XVI', 'Zn XVII', 'Zn XVIII', 'Zn XIX', 'Zn XX', 'Zn II', 'Zn XXI', 'Zn XXII', 'Zn XXIII', 'Zn XXIV', 'Zn XXV', 'Zn XXVI', 'Zn XXVII', 'Zn XXVIII', 'Zn XXIX', 'Zn XXX', 'Zn III', 'Zn XXXI', 'Zn IV', 'Zn V', 'Zn VI', 'Zn VII', 'Zn VIII', 'Zn IX', 'Zn X']
-allions = ['Al II', 'Al III', 'Ar I', 'Ar II', 'Ar VII', 'C I', 'C II', 'C III', 'C IV', 'Ca X', 'Fe II', 'H I', 'Mg II', 'Mg X', 'N I', 'N II', 'N III', 'N IV', 'N V', 'Na IX', 'Ne V', 'Ne VI', 'Ne VII', 'Ne VIII', 'O I', 'O II', 'O III', 'O IV', 'O V', 'O VI', 'P IV', 'P V', 'S II', 'S III', 'S IV', 'S V', 'S VI', 'S XIV', 'Si II', 'Si III', 'Si IV', 'Si XII']
-joeions = ['C III', 'H I', 'Mg II', 'Mg X', 'N II', 'N III', 'N IV', 'N V', 'Ne VIII', 'O II', 'O III', 'O IV', 'O V', 'O VI', 'S II', 'S III', 'S IV', 'S V', 'S VI']
+truly_allions = ['Al I', 'Al XI', 'Al XII', 'Al XIII', 'Al XIV', 'Al II', 'Al III', 'Al IV', \
+'Al V', 'Al VI', 'Al VII', 'Al VIII', 'Al IX', 'Al X', 'Ar I', 'Ar XI', 'Ar XII', 'Ar XIII', \
+'Ar XIV', 'Ar XV', 'Ar XVI', 'Ar XVII', 'Ar XVIII', 'Ar XIX', 'Ar II', 'Ar III', 'Ar IV', \
+'Ar V', 'Ar VI', 'Ar VII', 'Ar VIII', 'Ar IX', 'Ar X', 'B I', 'B II', 'B III', 'B IV', \
+'B V', 'B VI', 'Be I', 'Be II', 'Be III', 'Be IV', 'Be V', 'C I', 'C II', 'C III', 'C IV', \
+'C V', 'C VI', 'C VII', 'Ca I', 'Ca XI', 'Ca XII', 'Ca XIII', 'Ca XIV', 'Ca XV', 'Ca XVI', \
+'Ca XVII', 'Ca XVIII', 'Ca XIX', 'Ca XX', 'Ca II', 'Ca XXI', 'Ca III', 'Ca IV', 'Ca V', \
+'Ca VI', 'Ca VII', 'Ca VIII', 'Ca IX', 'Ca X', 'Cl I', 'Cl XI', 'Cl XII', 'Cl XIII', 'Cl XIV', \
+'Cl XV', 'Cl XVI', 'Cl XVII', 'Cl XVIII', 'Cl II', 'Cl III', 'Cl IV', 'Cl V', 'Cl VI', 'Cl VII',\
+'Cl VIII', 'Cl IX', 'Cl X', 'Co I', 'Co XI', 'Co XII', 'Co XIII', 'Co XIV', 'Co XV', 'Co XVI',\
+'Co XVII', 'Co XVIII', 'Co XIX', 'Co XX', 'Co II', 'Co XXI', 'Co XXII', 'Co XXIII', 'Co XXIV',\
+'Co XXV', 'Co XXVI', 'Co XXVII', 'Co XXVIII', 'Co III', 'Co IV', 'Co V', 'Co VI', 'Co VII',\
+'Co VIII', 'Co IX', 'Co X', 'Cr I', 'Cr XI', 'Cr XII', 'Cr XIII', 'Cr XIV', 'Cr XV', 'Cr XVI',\
+'Cr XVII', 'Cr XVIII', 'Cr XIX', 'Cr XX', 'Cr II', 'Cr XXI', 'Cr XXII', 'Cr XXIII', 'Cr XXIV',\
+'Cr XXV', 'Cr III', 'Cr IV', 'Cr V', 'Cr VI', 'Cr VII', 'Cr VIII', 'Cr IX', 'Cr X', 'Cu I',\
+'Cu XI', 'Cu XII', 'Cu XIII', 'Cu XIV', 'Cu XV', 'Cu XVI', 'Cu XVII', 'Cu XVIII', 'Cu XIX', \
+'Cu XX', 'Cu II', 'Cu XXI', 'Cu XXII', 'Cu XXIII', 'Cu XXIV', 'Cu XXV', 'Cu XXVI', 'Cu XXVII',\
+'Cu XXVIII', 'Cu XXIX', 'Cu XXX', 'Cu III', 'Cu IV', 'Cu V', 'Cu VI', 'Cu VII', 'Cu VIII', \
+'Cu IX', 'Cu X', 'F I', 'F II', 'F III', 'F IV', 'F V', 'F VI', 'F VII', 'F VIII', 'F IX', \
+'F X', 'Fe I', 'Fe XI', 'Fe XII', 'Fe XIII', 'Fe XIV', 'Fe XV', 'Fe XVI', 'Fe XVII', 'Fe XVIII',\
+'Fe XIX', 'Fe XX', 'Fe II', 'Fe XXI', 'Fe XXII', 'Fe XXIII', 'Fe XXIV', 'Fe XXV', 'Fe XXVI',\
+'Fe XXVII', 'Fe III', 'Fe IV', 'Fe V', 'Fe VI', 'Fe VII', 'Fe VIII', 'Fe IX', 'Fe X', 'H I',\
+'H II', 'He I', 'He II', 'He III', 'K I', 'K XI', 'K XII', 'K XIII', 'K XIV', 'K XV', 'K XVI',\
+'K XVII', 'K XVIII', 'K XIX', 'K XX', 'K II', 'K III', 'K IV', 'K V', 'K VI', 'K VII', 'K VIII',\
+'K IX', 'K X', 'Li I', 'Li II', 'Li III', 'Li IV', 'Mg I', 'Mg XI', 'Mg XII', 'Mg XIII', 'Mg II',\
+'Mg III', 'Mg IV', 'Mg V', 'Mg VI', 'Mg VII', 'Mg VIII', 'Mg IX', 'Mg X', 'Mn I', 'Mn XI',\
+'Mn XII', 'Mn XIII', 'Mn XIV', 'Mn XV', 'Mn XVI', 'Mn XVII', 'Mn XVIII', 'Mn XIX', 'Mn XX',\
+'Mn II', 'Mn XXI', 'Mn XXII', 'Mn XXIII', 'Mn XXIV', 'Mn XXV', 'Mn XXVI', 'Mn III', 'Mn IV',\
+'Mn V', 'Mn VI', 'Mn VII', 'Mn VIII', 'Mn IX', 'Mn X', 'N I', 'N II', 'N III', 'N IV', 'N V',\
+'N VI', 'N VII', 'N VIII', 'Na I', 'Na XI', 'Na XII', 'Na II', 'Na III', 'Na IV', 'Na V',\
+'Na VI', 'Na VII', 'Na VIII', 'Na IX', 'Na X', 'Ne I', 'Ne XI', 'Ne II', 'Ne III', 'Ne IV',\
+'Ne V', 'Ne VI', 'Ne VII', 'Ne VIII', 'Ne IX', 'Ne X', 'Ni I', 'Ni XI', 'Ni XII', 'Ni XIII',\
+'Ni XIV', 'Ni XV', 'Ni XVI', 'Ni XVII', 'Ni XVIII', 'Ni XIX', 'Ni XX', 'Ni II', 'Ni XXI',\
+'Ni XXII', 'Ni XXIII', 'Ni XXIV', 'Ni XXV', 'Ni XXVI', 'Ni XXVII', 'Ni XXVIII', 'Ni XXIX', 'Ni III',\
+'Ni IV', 'Ni V', 'Ni VI', 'Ni VII', 'Ni VIII', 'Ni IX', 'Ni X', 'O I', 'O II', 'O III', 'O IV',\
+'O V', 'O VI', 'O VII', 'O VIII', 'O IX', 'P I', 'P XI', 'P XII', 'P XIII', 'P XIV', 'P XV',\
+'P XVI', 'P II', 'P III', 'P IV', 'P V', 'P VI', 'P VII', 'P VIII', 'P IX', 'P X', 'S I',\
+'S XI', 'S XII', 'S XIII', 'S XIV', 'S XV', 'S XVI', 'S XVII', 'S II', 'S III', 'S IV',\
+'S V', 'S VI', 'S VII', 'S VIII', 'S IX', 'S X', 'Sc I', 'Sc XI', 'Sc XII', 'Sc XIII',\
+'Sc XIV', 'Sc XV', 'Sc XVI', 'Sc XVII', 'Sc XVIII', 'Sc XIX', 'Sc XX', 'Sc II', 'Sc XXI',\
+'Sc XXII', 'Sc III', 'Sc IV', 'Sc V', 'Sc VI', 'Sc VII', 'Sc VIII', 'Sc IX', 'Sc X', 'Si I',\
+'Si XI', 'Si XII', 'Si XIII', 'Si XIV', 'Si XV', 'Si II', 'Si III', 'Si IV', 'Si V', 'Si VI',\
+'Si VII', 'Si VIII', 'Si IX', 'Si X', 'Ti I', 'Ti XI', 'Ti XII', 'Ti XIII', 'Ti XIV', 'Ti XV',\
+'Ti XVI', 'Ti XVII', 'Ti XVIII', 'Ti XIX', 'Ti XX', 'Ti II', 'Ti XXI', 'Ti XXII', 'Ti XXIII',\
+'Ti III', 'Ti IV', 'Ti V', 'Ti VI', 'Ti VII', 'Ti VIII', 'Ti IX', 'Ti X', 'V I', 'V XI',\
+'V XII', 'V XIII', 'V XIV', 'V XV', 'V XVI', 'V XVII', 'V XVIII', 'V XIX', 'V XX', 'V II',\
+'V XXI', 'V XXII', 'V XXIII', 'V XXIV', 'V III', 'V IV', 'V V', 'V VI', 'V VII', 'V VIII',\
+'V IX', 'V X', 'Zn I', 'Zn XI', 'Zn XII', 'Zn XIII', 'Zn XIV', 'Zn XV', 'Zn XVI', 'Zn XVII',\
+'Zn XVIII', 'Zn XIX', 'Zn XX', 'Zn II', 'Zn XXI', 'Zn XXII', 'Zn XXIII', 'Zn XXIV', 'Zn XXV',\
+'Zn XXVI', 'Zn XXVII', 'Zn XXVIII', 'Zn XXIX', 'Zn XXX', 'Zn III', 'Zn XXXI', 'Zn IV', 'Zn V',\
+'Zn VI', 'Zn VII', 'Zn VIII', 'Zn IX', 'Zn X']
+allions = ['Al II', 'Al III', 'Ar I', 'Ar II', 'Ar VII', 'C I', 'C II', 'C III', 'C IV', 'Ca X',\
+'Fe II', 'H I', 'Mg II', 'Mg X', 'N I', 'N II', 'N III', 'N IV', 'N V', 'Na IX', 'Ne V',\
+'Ne VI', 'Ne VII', 'Ne VIII', 'O I', 'O II', 'O III', 'O IV', 'O V', 'O VI', 'P IV',\
+'P V', 'S II', 'S III', 'S IV', 'S V', 'S VI', 'S XIV', 'Si II', 'Si III', 'Si IV', 'Si XII']
+joeions = ['C III', 'H I', 'Mg II', 'Mg X', 'N II', 'N III', 'N IV', 'N V', 'Ne VIII', 'O II', \
+'O III', 'O IV', 'O V', 'O VI', 'S II', 'S III', 'S IV', 'S V', 'S VI']
+alloxygens = ['O I', 'O II', 'O III', 'O IV', 'O V', 'O VI', 'O VII', 'O VIII', 'O IX', 'H I',\
+'H II']
+
