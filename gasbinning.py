fixed gasbins instantiation with units

gasbinning.py
@@ -45,6 +45,7 @@ density_bin = GasBin("density",["low","medium","selfshielding","starforming"],["
 temperature_bin = GasBin("temperature",["cold","cool","warm-hot","hot"],["0.0","10**3.8","10**4.5","10**6.5","np.inf"])
 radial_velocity_bin = GasBin("radial_velocity",["inflow","tangential","outflow"],['-np.inf','-10','10','np.inf'],units = "km/s")
 resolution_bin = GasBin("resolution",["high","medium","low"],['0.00e+00', '1.70e+05', '3.40e+05', 'np.inf'],field = ('gas','dx'),units = "cpc")
+possible_bin_types = ["density","temperature","radial_velocity","resolution"]
 
 
 class GasBinsHolder(object):
@@ -62,6 +63,7 @@ class GasBinsHolder(object):
                 currentBinVals = [allinfo[2].split("_")[0],allinfo[2].split("_")[1]]
                 j = 1
                 field = None
+                units = None
                 while i+j < len(allnames) and allnames[i+j].startswith(currentBin):
                     allinfo = allnames[i+j].split(":")
                     currentBinNames += [allinfo[1]]
@@ -74,17 +76,13 @@ class GasBinsHolder(object):
                             units = allinfo[4].split("-")[1]
                         elif "units" in allinfo[3]:
                             units = allinfo[3].split("-")[1]
-                    else:
-                        field = None
                     j+=1
                 i+=j
-                newBin = GasBin(currentBin,currentBinNames,currentBinVals,field = field)
+                newBin = GasBin(currentBin,currentBinNames,currentBinVals,field = field,units = units)
                 self.bin_types.append(newBin)
             return
-
-        self.possible_bin_types = ["density","temperature","radial_velocity","resolution"]
         if bins == "all":
-            bins = self.possible_bin_types
+            bins = possible_bin_types
         elif bins is None:
             bins = []
         if "density" in bins:

multi_quasar_sphere_plotter.py
@@ -5,6 +5,7 @@ import os
 import sys
 import matplotlib.pyplot as plt
 from quasar_scan import *
+import gasbinning
 from parse_vela_metadata import Rdict, Ldict
 
 #precondition: assumes there are only two levels of depth within the output folder
@@ -180,8 +181,18 @@ class MultiQuasarSpherePlotter():
             return np.array(nonemptyLabelArray),bins, np.array(nonemptyArray)
         print "Bins are empty." if empty else ""
         return labels,bins, quasarBins
-        
-    def constrain_current_Quasar_Array(self, constrainCriteria, bins, exploration_mode = False,atEnd = False,splitEven = None,extra_title = ""):
+    
+    def constrain_via_gasbins(self,gasbintype=None):
+        if gasbintype == None:
+            gasbintype = raw_input("Available bins are: %s"%gasbinning.possible_bin_types)
+        g = gasbinning.GasBinsHolder(bins=[gasbintype])
+        toReturn = []
+        for q in self.currentQuasarArray:
+            if g.get_bin_str() in q.gasbins.get_bin_str():
+                toReturn.append(q)
+        self.currentQuasarArray = toReturn
+    
+    def constrain_current_Quasar_Array(self, constrainCriteria, bins, exploration_mode = False,atEnd = False,splitEven = None):
         if len(self.currentQuasarArray)>0 and not (constrainCriteria in self.currentQuasarArray[0].__dict__.keys()):
             print ("Constrain criteria " + constrainCriteria + " does not exist. Please re-enter a valid criteria.")
             return

quasar_scan.py
@@ -175,7 +175,13 @@ class GeneralizedQuasarSphere(object):
 class QuasarSphere(GeneralizedQuasarSphere):
     def __init__(self,ions=None,simname=None,dspath=None,data = None,\
                  simparams = None,scanparams = None,Rvir = None,L=np.array([0,0,1]),\
-                 ytlevel = "quiet",readonly = False,gasbins = None):
+                 ytlevel = "quiet",readonly = False,gasbins = None,readvalsoutput = None):
+        if readvalsoutput:
+            simparams  = readvalsoutput[0]
+            scanparams = readvalsoutput[1]
+            ions       = readvalsoutput[2]
+            data       = readvalsoutput[3]
+            gasbins    = readvalsoutput[4]
         self.scan_version = "2.0"
         self.number = 1
         if ytlevel == "loud":