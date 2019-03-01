13dd9400b3d5516c7e1baef4e7dd8220e00b5ad6
Clayton Strawn <cjstrawn@ucsc.edu>
2019-02-28 16:22:48 -0800

    added new bash/slurm scripts

--- run_one_new_snapshot_nersc.sh
+++ run_one_new_snapshot_nersc.sh
@@ -8,8 +8,9 @@
 #SOURCEDIR2="/project/projectdirs/mp363/VELA_ANALYSIS"
 simname=$1
 filename=$2
+z=$3
 
 python quasarscan/create_qso_endpoints.py /global/cscratch1/sd/cstrawn/$filename $simname
-srun -n 32 -u python -u quasarscan/get_coldens.py quasarscan/output/${simname}coldensinfo/0_of_416-agoraions_z$z.txt 96 p
+srun -n 32 -u python -u quasarscan/get_coldens.py quasarscan/output/${simname}coldensinfo/0_of_448-agoraions_z$z.txt 96 p
 
 echo "remember to rm $filename"
