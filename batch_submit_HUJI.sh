e4d4068bbdbc9691548f30433b532bbcdafe7599
Clayton Strawn <cstrawn@cori13.nersc.gov>
2019-03-21 15:53:11 -0700

    made plots of phase distribution for Santi

--- batch_submit_HUJI.sh
+++ batch_submit_HUJI.sh
@@ -1,9 +1,10 @@
+
 #!/bin/bash -l
 
-#SBATCH -c 1         #Use 1 cpu
+#SBATCH -n 1         #Use 1 cpu
 #SBATCH -t 02:00:00  #Set 6 hr time limit
 #SBATCH --mem 400m   #400 MB of memory per node
-#SBATCH -n 10        #perform 10 tasks at once
+#SBATCH -c 10        #perform 10 tasks at once
 
 
 export HDF5_USE_FILE_LOCKING=FALSE
