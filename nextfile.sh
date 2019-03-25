039e104e77e89095dc197399f6b2248edb4211ad
Clayton Strawn <cstrawn@cori19.nersc.gov>
2019-03-25 11:54:15 -0700

    hopefully, bash bug is fixed in run_one_snapshot

--- nextfile.sh
+++ nextfile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-quasarscan/./run_one_new_snapshot_nersc.sh VELA_v2_art_25 /global/cscratch1/sd/cstrawn/VELA_v2_25/10MpcBox_csf512_a0.250.d 3.0 192
+quasarscan/./run_one_new_snapshot_nersc.sh VELA_v2_art_21 /global/cscratch1/sd/cstrawn/VELA_v2_21/10MpcBox_csf512_a0.500.d 1.0 0
--- run_one_new_snapshot_nersc.sh
+++ run_one_new_snapshot_nersc.sh
@@ -5,8 +5,11 @@ filename=$2
 z=$3
 n=$4
 
-python quasarscan/create_qso_endpoints.py $simname $filename
+if (($n == '0')); then
+    python quasarscan/create_qso_endpoints.py $simname $filename
+fi
 srun -n 32 -u python -u quasarscan/get_coldens.py quasarscan/output/${simname}coldensinfo/${n}_of_448-agoraions_z${z}.txt 96 p
+
 #in case you want to run one line interactively:
 #srun -n 32 -u python -u quasarscan/get_coldens.py quasarscan/output/AGORA_v1_art_01coldensinfo/96_of_448-agoraions_z15.0.txt 96 p
 #python quasarscan/get_coldens.py quasarscan/output/${simname}coldensinfo/0_of_448-agoraions_z$z.txt 96 p
