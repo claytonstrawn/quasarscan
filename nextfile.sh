e24eda7af7a756ed01e107db00b5dd92842c4c41
Clayton Strawn <cstrawn@cori19.nersc.gov>
2019-04-03 13:35:22 -0700

    stopped tracking untracked files

--- nextfile.sh
+++ nextfile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-quasarscan/./run_one_new_snapshot_nersc.sh VELA_v2_art_19 /global/cscratch1/sd/cstrawn/VELA_v2_19/10MpcBox_csf512_a0.200.d 4.0 0
+quasarscan/./run_one_new_snapshot_nersc.sh VELA_v2_art_20 /global/cscratch1/sd/cstrawn/VELA_v2_20/10MpcBox_csf512_a0.250.d 3.0 0
