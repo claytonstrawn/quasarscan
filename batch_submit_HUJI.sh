b414964bd3e9ec52c18ab19c52c2fff7971ae6b4
Clayton Strawn <cjstrawn@cs.huji.ac.il>
2019-05-14 02:00:52 +0300

    recorded some data from nihao14

--- batch_submit_HUJI.sh
+++ batch_submit_HUJI.sh
@@ -1,11 +1,11 @@
-
 #!/bin/bash -l
 
 #SBATCH -n 1         #Use 1 cpu
 #SBATCH -t 02:00:00  #Set 6 hr time limit
 #SBATCH --mem 400m   #400 MB of memory per node
-#SBATCH -c 10        #perform 10 tasks at once
+#SBATCH -c 1        #perform 10 tasks at once
 
 
 export HDF5_USE_FILE_LOCKING=FALSE
+source myenv/bin/activate.csh
 quasarscan/./test_on_huji.sh
