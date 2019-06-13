3aef0137d649d85d14539a89d82769d157a4bbe6
Clayton Strawn <cjstrawn@cs.huji.ac.il>
2019-06-13 23:45:14 +0300

    clearing up last things from huji

--- batch_submit_HUJI.sh
+++ batch_submit_HUJI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 #SBATCH -n 1         #Use 1 cpu
-#SBATCH -t 02:00:00  #Set 6 hr time limit
+#SBATCH -t 04:00:00  #Set 6 hr time limit
 #SBATCH --mem 400m   #400 MB of memory per node
 #SBATCH -c 1        #perform 10 tasks at once
 
--- test_on_huji.sh
+++ test_on_huji.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python quasarscan/create_qso_endpoints.py NIHAO_v1_tipsy_13 files_to_process/g8.13e11.00112
-python quasarscan/get_coldens.py quasarscan/output/NIHAO_v1_tipsy_13coldensinfo/0_of_448-agoraions_z4.0.txt 96 n
-python quasarscan/create_qso_endpoints.py NIHAO_v1_tipsy_13 files_to_process/g8.13e11.00160
-python quasarscan/get_coldens.py quasarscan/output/NIHAO_v1_tipsy_13coldensinfo/0_of_448-agoraions_z3.0.txt 96 n
-python quasarscan/create_qso_endpoints.py NIHAO_v1_tipsy_13 files_to_process/g8.13e11.00192
-python quasarscan/get_coldens.py quasarscan/output/NIHAO_v1_tipsy_13coldensinfo/0_of_448-agoraions_z2.5.txt 96 n
+#python quasarscan/create_qso_endpoints.py NIHAO_v1_tipsy_13 files_to_process/g8.13e11.00112
+#python quasarscan/get_coldens.py quasarscan/output/NIHAO_v1_tipsy_13coldensinfo/0_of_448-agoraions_z4.0.txt 96 n
+#python quasarscan/create_qso_endpoints.py NIHAO_v1_tipsy_13 files_to_process/g8.13e11.00160
+#python quasarscan/get_coldens.py quasarscan/output/NIHAO_v1_tipsy_13coldensinfo/0_of_448-agoraions_z3.0.txt 96 n
+#python quasarscan/create_qso_endpoints.py NIHAO_v1_tipsy_13 files_to_process/g8.13e11.00192
+python quasarscan/get_coldens.py quasarscan/output/NIHAO_v1_tipsy_13coldensinfo/288_of_448-agoraions_z2.5.txt 96 n
 python quasarscan/create_qso_endpoints.py NIHAO_v1_tipsy_13 files_to_process/g8.13e11.00240
 python quasarscan/get_coldens.py quasarscan/output/NIHAO_v1_tipsy_13coldensinfo/0_of_448-agoraions_z2.0.txt 96 n
 python quasarscan/create_qso_endpoints.py NIHAO_v1_tipsy_13 files_to_process/g8.13e11.00320
