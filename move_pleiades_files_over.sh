457fda9ca8004ca40a15225989978149924c5ce6
Clayton Strawn <cjstrawn@ucsc.edu>
2019-06-25 12:54:44 -0700

    switched to faster interNAS transfer mechanism

--- move_pleiades_files_over.sh
+++ move_pleiades_files_over.sh
@@ -8,13 +8,13 @@ echo "entering directory VELA$VELANUM"
 
 for d in 500 400 330 250 200; do
 	echo "copying 10MpcBox_csf512_a0.$d.d..."
-    cp 10MpcBox_csf512_a0.$d.d /nobackupp2/cstrawn/mydir/VELA${VELANUM}v2
+    shiftc 10MpcBox_csf512_a0.$d.d /nobackupp2/cstrawn/mydir/VELA${VELANUM}v2
 	echo "copying PMcrda0.$d.DAT..."
-    cp PMcrda0.$d.DAT /nobackupp2/cstrawn/mydir/VELA${VELANUM}v2
+    shiftc PMcrda0.$d.DAT /nobackupp2/cstrawn/mydir/VELA${VELANUM}v2
 	echo "copying PMcrs0a0.$d.DAT..."
-    cp PMcrs0a0.$d.DAT /nobackupp2/cstrawn/mydir/VELA${VELANUM}v2
+    shiftc PMcrs0a0.$d.DAT /nobackupp2/cstrawn/mydir/VELA${VELANUM}v2
 	echo "copying pta0.$d.DAT..."
-    cp pta0.$d.dat /nobackupp2/cstrawn/mydir/VELA${VELANUM}v2
+    shiftc pta0.$d.dat /nobackupp2/cstrawn/mydir/VELA${VELANUM}v2
 	echo "copying stars_a0.$d.dat..."
-    cp stars_a0.$d.dat /nobackupp2/cstrawn/mydir/VELA${VELANUM}v2
+    shiftc stars_a0.$d.dat /nobackupp2/cstrawn/mydir/VELA${VELANUM}v2
 done
