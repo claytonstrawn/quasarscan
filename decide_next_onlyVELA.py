33f2ffd151a0eb2a4983cfdc73320cf915b26f26
Clayton Strawn <cjstrawn@ucsc.edu>
2019-06-25 12:32:14 -0700

    prepared decide_next to use all vela sims incl NASA ones

--- decide_next_onlyVELA.py
+++ decide_next_onlyVELA.py
@@ -6,8 +6,8 @@ from ion_lists import *
 
 alltextfiles = get_all_textfiles(False)
 
-#NOTE: missing [7,8,9,10,11,12,13,14,15,18]
-numsnersc = [1,2,3,4,5,6,16,17,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
+#NOTE: need to bring [7,8,9,10,11,12,13,14,15,18] from Pleiades
+numsnersc = range(1,36)
 
 nerscsimnames = []
 for num in numsnersc:
