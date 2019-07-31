f7abe23548098ec76483a09555018a010a7e0033
Clayton Strawn <cjstrawn@cs.huji.ac.il>
2019-07-31 21:01:07 +0300

    some of NIHAO02 done, also removed PI stuff temporarily

--- nextfile.sh
+++ nextfile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-quasarscan/batch_scripts/./run_one_new_snapshot_huji.sh NIHAO_v1_tipsy_01 /vol/sci/astro/home/cjstrawn/files_to_process/NIHAO_v1_tipsy_01/g2.63e10.00432 1.0 0
+quasarscan/batch_scripts/./run_one_new_snapshot_huji.sh NIHAO_v1_tipsy_02 /vol/sci/astro/home/cjstrawn/files_to_process/NIHAO_v1_tipsy_02/g2.19e11.00320 1.5 256
