327c3a75f683f1d82efbf396072afb63f7f1a358
Clayton Strawn <cjstrawn@cs.huji.ac.il>
2019-08-01 20:03:06 +0300

    took more data from nihao02

--- nextfile.sh
+++ nextfile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-quasarscan/batch_scripts/./run_one_new_snapshot_huji.sh NIHAO_v1_tipsy_02 /vol/sci/astro/home/cjstrawn/files_to_process/NIHAO_v1_tipsy_02/g2.19e11.00320 1.5 256
+quasarscan/batch_scripts/./run_one_new_snapshot_huji.sh NIHAO_v1_tipsy_02 /vol/sci/astro/home/cjstrawn/files_to_process/NIHAO_v1_tipsy_02/g2.19e11.00112 4.0 0
