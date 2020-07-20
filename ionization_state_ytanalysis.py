63bbbe0f6fffdbc1d63b0aac59089f92f15db563
Clayton Strawn <cjstrawn@ucsc.edu>
2020-07-20 14:02:30 -0700

    created template file for SIP intern script

---
+++ ionization_state_ytanalysis.py
@@ -0,0 +1,26 @@
+#***import statements***
+import sys
+
+#define analysis functions
+
+def rahuls_function(galaxy_name,galaxy_file_loc,options):
+	#do things
+	#save some images and 
+	#save some raw data values
+
+def sallys_function(galaxy_name,galaxy_file_loc,options):
+	#do other things
+	#save some images and 
+	# save some raw data values
+
+if __name__ == '__main__':
+	# if you're running this from terminal like "python ionization_state_ytanalysis.py galname file_loc"
+	galaxy_name = sys.argv[1]
+	galaxy_file_loc = sys.argv[2]
+	rahuls_function(galaxy_name,galaxy_file_loc)
+	sallys_function(galaxy_name,galaxy_file_loc)
+
+
+
+
+
