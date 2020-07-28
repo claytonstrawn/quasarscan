026af9df688050dfc4acde8db838019638f8840c
Sally Zhu <sallyzhu57>
2020-07-28 11:00:19 -0700

    added datetime between simulations

--- ion_state_run_simulations_ytanalysis.py
+++ ion_state_run_simulations_ytanalysis.py
@@ -6,6 +6,8 @@ import sys
 # import PIL
 # from PIL import Image
 import os
+import datetime
+import pytz
 # import shutil
 # import parse_metadata
 import os.path
@@ -13,12 +15,11 @@ from os import path
 from ionization_state_ytanalysis import *
 
 def run_functions():
-#     simulations = ["VELA_v2_art_01", "VELA_v2_art_02", "VELA_v2_art_03", "VELA_v2_art_04", "VELA_v2_art_05", "VELA_v2_art_16",
-#                    "VELA_v2_art_17", "VELA_v2_art_18", "VELA_v2_art_19", "VELA_v2_art_20", "VELA_v2_art_21", "VELA_v2_art_22",
-#                    "VELA_v2_art_23", "VELA_v2_art_24", "VELA_v2_art_25", "VELA_v2_art_26", "VELA_v2_art_27", "VELA_v2_art_28",
-#                    "VELA_v2_art_29", "VELA_v2_art_30", "VELA_v2_art_31", "VELA_v2_art_32", "VELA_v2_art_33", "VELA_v2_art_34",
-#                    "VELA_v2_art_35"]
-    simulations = ["VELA_v2_art_01", "VELA_v2_art_02", "VELA_v2_art_03", "VELA_v2_art_04"]
+    simulations = ["VELA_v2_art_01", "VELA_v2_art_02", "VELA_v2_art_03", "VELA_v2_art_04", "VELA_v2_art_05", "VELA_v2_art_16",
+                   "VELA_v2_art_17", "VELA_v2_art_18", "VELA_v2_art_19", "VELA_v2_art_20", "VELA_v2_art_21", "VELA_v2_art_22",
+                   "VELA_v2_art_23", "VELA_v2_art_24", "VELA_v2_art_25", "VELA_v2_art_26", "VELA_v2_art_27", "VELA_v2_art_28",
+                   "VELA_v2_art_29", "VELA_v2_art_30", "VELA_v2_art_31", "VELA_v2_art_32", "VELA_v2_art_33", "VELA_v2_art_34",
+                   "VELA_v2_art_35"]
     files = []
     file_exists = []
     already_done = []
@@ -32,6 +33,8 @@ def run_functions():
         already_done.append(path.exists(filename))
 
     for i in range(len(simulations)):
+        current_time = datetime.datetime.now(pytz.timezone('America/Los_Angeles'))
+        print("Time for simulation " + simulations[i] + " before Rahul's function: " + str(current_time))
         if(file_exists[i] == True and already_done[i] == False):
             # edit this if red shift is changed!!
             rahuls_function(simulations[i],files[i],1.0)
@@ -40,7 +43,11 @@ def run_functions():
                 new_file = '10MpcBox_csf512_a0.500.d_Projection_x_O_p' + str(j) + '_number_density.png'
                 if(os.path.exists(new_file)):
                     os.remove(new_file)
+            current_time = datetime.datetime.now(pytz.timezone('America/Los_Angeles'))
+            print("Time for simulation " + simulations[i] + " after Rahul's function: " + str(current_time))
             sallys_function(simulations[i],files[i],1.0)
+            current_time = datetime.datetime.now(pytz.timezone('America/Los_Angeles'))
+            print("Time for simulation " + simulations[i] + " after Sally's function: " + str(current_time))
         elif(already_done[i] == True): 
             print(simulations[i] + " was already done")
         else:
