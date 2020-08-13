37a67822d50fd32986fbbd44c06cb915f82c5d7d
Sally Zhu <sallyzhu57>
2020-08-13 09:53:54 -0700

    added VELA 07, 10 to ion_state_ytanalysis folder

--- ion_state_run_simulations_ytanalysis.py
+++ ion_state_run_simulations_ytanalysis.py
@@ -7,16 +7,19 @@ from os import path
 from ionization_state_ytanalysis import *
 
 def run_functions():
-    simulations = ["VELA_v2_art_01", "VELA_v2_art_02", "VELA_v2_art_03", "VELA_v2_art_04", "VELA_v2_art_05", "VELA_v2_art_07", 
-                   "VELA_v2_art_08", "VELA_v2_art_10", "VELA_v2_art_16", "VELA_v2_art_17", "VELA_v2_art_18", "VELA_v2_art_19", 
-                   "VELA_v2_art_20", "VELA_v2_art_21", "VELA_v2_art_22", "VELA_v2_art_23", "VELA_v2_art_24", "VELA_v2_art_25", 
-                   "VELA_v2_art_26", "VELA_v2_art_27", "VELA_v2_art_28", "VELA_v2_art_29", "VELA_v2_art_30", "VELA_v2_art_31", 
-                   "VELA_v2_art_32", "VELA_v2_art_33", "VELA_v2_art_34", "VELA_v2_art_35"]
+#     simulations = ["VELA_v2_art_01", "VELA_v2_art_02", "VELA_v2_art_03", "VELA_v2_art_04", "VELA_v2_art_05", "VELA_v2_art_07", 
+#                    "VELA_v2_art_08", "VELA_v2_art_10", "VELA_v2_art_16", "VELA_v2_art_17", "VELA_v2_art_18", "VELA_v2_art_19", 
+#                    "VELA_v2_art_20", "VELA_v2_art_21", "VELA_v2_art_22", "VELA_v2_art_23", "VELA_v2_art_24", "VELA_v2_art_25", 
+#                    "VELA_v2_art_26", "VELA_v2_art_27", "VELA_v2_art_28", "VELA_v2_art_29", "VELA_v2_art_30", "VELA_v2_art_31", 
+#                    "VELA_v2_art_32", "VELA_v2_art_33", "VELA_v2_art_34", "VELA_v2_art_35"]
     files = []
     file_exists = []
     already_done = []
-    redshifts = [1.0, 2.0]
-    names = ["10MpcBox_csf512_a0.500.d", "10MpcBox_csf512_a0.400.d"]
+    #redshifts = [1.0, 2.0]
+    #names = ["10MpcBox_csf512_a0.500.d", "10MpcBox_csf512_a0.400.d"]
+    simulations = ["VELA_v2_art_08"]
+    redshifts = [1.0]
+    names = ["10MpcBox_csf512_a0.460.d"]
 
     for z in range(len(redshifts)):
         print("Starting redshift = " + str(redshifts[z]))
@@ -24,10 +27,8 @@ def run_functions():
         file_exists = []
         already_done = []
         for i in range(len(simulations)):
-            # edit this if redshift is changed!!
             files.append("/global/cfs/cdirs/mp363/SIP_INTERNS_2020/" + simulations[i] + '/' + names[z])
             file_exists.append(path.exists(files[i]))
-            # edit this if redshift is changed!!
             filename = "quasarscan/ion_state_ytanalysis/" + simulations[i] + "_" + str(redshifts[z]) + "/o_ion_fraction_plot.png"
             already_done.append(path.exists(filename))
 
@@ -36,10 +37,8 @@ def run_functions():
             print("Time for simulation " + simulations[i] + " before Rahul's function: " + str(current_time))
             if(file_exists[i] == True and already_done[i] == False):
                 ds, redshift, Rvir, Mvir, center = setup(simulations[i],files[i],redshifts[z])
-                # edit this if red shift is changed!!
                 rahuls_function(simulations[i],files[i], ds, redshift, Rvir, Mvir, center)
                 for j in range(9):
-                    # edit this if redshift is changed!!
                     new_file = names[z] + '_Projection_x_O_p' + str(j) + '_number_density.png'
                     if(os.path.exists(new_file)):
                         os.remove(new_file)
@@ -51,7 +50,6 @@ def run_functions():
                         os.remove(new_file)
                 current_time = datetime.datetime.now(pytz.timezone('America/Los_Angeles'))
                 print("Time for simulation " + simulations[i] + " after Rahul's function: " + str(current_time))
-                # edit this if redshift is changed!!
                 sallys_function(simulations[i],files[i],ds, redshift, Rvir, Mvir, center)
                 current_time = datetime.datetime.now(pytz.timezone('America/Los_Angeles'))
                 print("Time for simulation " + simulations[i] + " after Sally's function: " + str(current_time))
